b224600e71545ff14e75978936c7000b71a327f7
John Doe <john.doe@gmail.com>
2026-03-21 17:10:17 +0300

    updated the login without css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,40 @@
+// import {useState} from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import axios from 'axios'
+
 export default function Login() {
-  return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  );
-}
+ return (
+<div className='login'>
+  <h2>Login</h2>
+<form>
+  <input 
+  type='email'
+  name='email'
+  placeholder='Email'
+
+  required
+/>
+<input 
+  type='password'
+  name='password'
+  placeholder='Enter Password'
+ 
+  required
+/>
+
+
+
+<button type='Submit'>Login</button>
+
+</form>
+  
+<p style ={{color:'red'}}>
+  Fill in all the fields to login
+</p>
+
+<p>
+  Dont have an account? <a href='/register'>Sign Up</a>
+</p>
+
+</div>
+ );}
